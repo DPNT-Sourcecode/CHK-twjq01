1de9531f5114f1610059bcc793bb4049cfa21d3c
sbx_user1051 <sbx_user1051@169.254.182.53>
2019-06-27 14:48:55 +0000

    Thu Jun 27 14:48:55 UTC 2019

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,23 @@
+package befaster.solutions.CHK;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckoutSolutionTest {
+	
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp() {
+
+    	checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout() {
+    	// assertThat(hello.hello("ValTech"), equalTo("Hello, ValTech!"));
+    }
+}

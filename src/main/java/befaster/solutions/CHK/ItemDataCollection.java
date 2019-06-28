fdc9bdbe3cff9fa4f4e7d31fa64955b5a8fb47bf
sbx_user1051 <sbx_user1051@169.254.44.49>
2019-06-28 09:04:30 +0000

    Fri Jun 28 09:04:30 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/ItemDataCollection.java
@@ -0,0 +1,17 @@
+package befaster.solutions.CHK;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemDataCollection {
+	
+	
+	public static Map<String, Long> getItemQuantityMap() {
+		Map<String, Long> map = new HashMap<>();
+		
+		
+		
+		return map;
+	}
+
+}

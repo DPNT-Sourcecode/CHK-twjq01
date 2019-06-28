789f8cb91247eeb09440645e6da0296956b9bf40
sbx_user1051 <sbx_user1051@169.254.44.49>
2019-06-28 09:13:30 +0000

    Fri Jun 28 09:13:30 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,6 +2,7 @@ package befaster.solutions.CHK;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
@@ -18,12 +19,15 @@ public class CheckoutSolution {
     		List<String> shoppingCartItemList = Arrays.stream(skus.split("\\s*,\\s*")).collect(Collectors.toList());
     		
     		// Prepare Item Quantity Map here
+    		Map<String, Long> itemQuantityMap = ItemDataCollection.getItemQuantityMap(shoppingCartItemList);
     		
+    		// Now Calculate Total Price
     	}
     	
     	return totalPrice;
     }
 }
+
 
 
 
--- src/main/java/befaster/solutions/CHK/ItemDataCollection.java
+++ src/main/java/befaster/solutions/CHK/ItemDataCollection.java
@@ -1,23 +1,23 @@
 package befaster.solutions.CHK;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.collections.CollectionUtils;
 
 public class ItemDataCollection {
 	
-	
 	public static Map<String, Long> getItemQuantityMap(List<String> shoppingCartItemList) {
-		Map<String, Long> map = new HashMap<>();
-		
 		if(CollectionUtils.isNotEmpty(shoppingCartItemList)) {
-			
+			return shoppingCartItemList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		}else {
+			return Collections.emptyMap();
 		}
-		
-		return map;
 	}
 
 }
+
 

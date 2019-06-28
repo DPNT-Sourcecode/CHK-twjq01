d187720de170e54c2b8243cbd83aaf8316a037a1
sbx_user1051 <sbx_user1051@169.254.177.253>
2019-06-28 22:47:35 +0000

    Fri Jun 28 22:47:35 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -31,7 +31,8 @@ public class CheckoutSolutionTest {
 //		assertThat(checkout.checkout("EEEEBB"), equalTo(160));
 //		assertThat(checkout.checkout("BEBEEE"), equalTo(160));
 //		assertThat(checkout.checkout("ABCDECBAABCABBAAAEEAA"), equalTo(650));
-		assertThat(checkout.checkout("ABCDE"), equalTo(155));
+		assertThat(checkout.checkout("ABCDEABCDE"), equalTo(255));
 	}
 }
+
 
